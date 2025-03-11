0eb34361ff4ae403bc740fed61272afa6c479a53
Mackey Nolan <nolan.mackey@etu.univ-orleans.fr>
2025-03-11 16:23:44 +0100

    Version_final

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -30,9 +30,9 @@ public class Controleur {
         // Parcourir le plateau pour compter les pions
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (ihm.getPlateau().getCase(i,j) == 1) {
+                if (ihm.getPlateau().getCase(i,j) == 2) {
                     scoreJoueur1++; // Incrementer le score du joueur 1
-                } else if (ihm.getPlateau().getCase(i,j) == 2) {
+                } else if (ihm.getPlateau().getCase(i,j) == 1) {
                     scoreJoueur2++; // Incrementer le score du joueur 2
                 }
             }
@@ -66,7 +66,7 @@ public class Controleur {
         while(PartieEnCours){
             //ihm.getPlateau().setCase(5, 5, 2);
             
-            ihm.afficherMessage(joueurActuel.getNom() + " à vous de jouer. Saisir une ligne entre 1 et 8 suivie d'une lettre entre 1 et 8 ou P pour passer le tour");
+            ihm.afficherMessage(joueurActuel.getNom() + " à vous de jouer. Saisir une colonne entre A et H suivie d'une ligne entre 1 et 8 ou P pour passer le tour");
             if ( !(ihm.getPlateau().aUnCoupValide(joueurActuel))){
                 String coup=ihm.demanderCaracteres("Aucun coup valide disponible, appuyez sur P pour passer votre tour");
                 if (coup=="P" || coup=="p"){
@@ -76,8 +76,14 @@ public class Controleur {
                     else{joueurActuel=joueur1;}
                 }
             }else{
-                int x = ihm.demanderEntier("Entrez la ligne (1-8) : "); 
-                int y = ihm.demanderEntier("Entrez la colonne (1-8) : ");//ptete faire un case pour A-H
+                String coup = ihm.demanderCaracteres("Entrez le chiffre de la ligne suivie d'une virgule et d'une lettre pour la ligne (3,D)");
+                
+                String[] lescoups = coup.split(",");
+                if (lescoups.length!=2){ihm.afficherMessage("Entrée Invalide! Veuillez entrez le chiffre de la ligne suivie d'une virgule et d'une lettre pour la ligne (3,D)");}
+                int x = Integer.parseInt(lescoups[0]);//ligne 
+                int y = ihm.getPlateau().convertirLettretoChiffre(lescoups[1]);//la colonne
+                //int x = ihm.demanderEntier("Entrez la ligne (1-8) : "); 
+                //int y = ihm.demanderEntier("Entrez la colonne (1-8) : ");//ptete faire un case pour A-H
                 if (ihm.getPlateau().estCoupValide(joueurActuel, x, y)){//normalement marche
                     ihm.getPlateau().setCase(x, y, joueurActuel.getTypedepion()=="\u26AA"?1:2);//censé etre ok
                     ihm.getPlateau().retournerPions(joueurActuel, x, y);//marche pas//good aussi
@@ -91,22 +97,27 @@ public class Controleur {
             if (!(ihm.getPlateau().aUnCoupValide(joueur1)&&ihm.getPlateau().aUnCoupValide(joueur2))){//à voir mais normalement c'est ok
                 PartieEnCours=false;
             }
+            //PartieEnCours=false;// à enlever
         }
         ihm.afficherMessage("La Partie est terminée !");
 
         if(determinerGagnant()==joueur1.getNom()){
-            ihm.afficherMessage(joueur1.getNom()+"a gagner cette partie !");
+            ihm.afficherMessage(joueur1.getNom()+" a gagné cette partie !");
             joueur1.upScore();
         }else if(determinerGagnant()==joueur2.getNom()){
-            ihm.afficherMessage(joueur2.getNom()+"a gagner cette partie !");
+            ihm.afficherMessage(joueur2.getNom()+" a gagné cette partie !");
             joueur2.upScore();
         }else{ihm.afficherMessage("Ex aequo");}
            
         String rejouer=ihm.demanderCaracteres("Voulez vous rejouer, Y or N ?");
-        ilsveulentjouer=(rejouer=="Y"?true:false);
+        if (rejouer.equalsIgnoreCase("Y")){
+            ilsveulentjouer=true;
+        }else {ilsveulentjouer=false;}
+        //System.out.println(ilsveulentjouer);  test la value de si on rejoue
+        //ilsveulentjouer=((rejouer=="Y" || rejouer=="y" )?true:false);
     }
 
-    ihm.afficherMessage(joueur1.getNom()+" a gagner "+joueur1.getScore()+"parties ! Et "+ joueur2.getNom() +" a gagner "+joueur2.getScore()+"parties !");
+    ihm.afficherMessage(joueur1.getNom()+" a gagné "+joueur1.getScore()+" parties ! Et "+ joueur2.getNom() +" a gagné "+joueur2.getScore()+" parties !");
     if(joueur1.getScore()==joueur2.getScore()){ihm.afficherMessage("C'est ex aequo , il n'y a pas de vainqueur");
     }else if(joueur1.getScore()>=joueur2.getScore()){ihm.afficherMessage("C'est "+joueur1.getNom()+" qui a gagné");
     }else{ihm.afficherMessage("C'est "+joueur2.getNom()+" qui a gagné");}
--- src/modele/Plateau.java
+++ src/modele/Plateau.java
@@ -149,4 +149,29 @@ public class Plateau {
         }
         return false; //0 coup valide
     }
+
+    public int convertirLettretoChiffre(String lettre){
+        switch (lettre) {
+            case "A":
+                return 1;
+            case "B":
+                return 2;
+            case "C":
+                return 3;
+            case "D":
+                return 4;
+            case "E":
+                return 5;
+            case "F":
+                return 6;
+            case "G":
+                return 7;
+            case "H":
+                return 8;
+            default:
+                return -1;
+            
+
+        }
+    }
 }
